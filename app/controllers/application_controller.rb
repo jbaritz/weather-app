6725072761a6352a1f539fb81503a01c3dac9f78
jbaritz <jbaritz@gmail.com>
2025-11-14 21:28:04 -0600

    refactor controller logic into helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,33 +8,21 @@ class ApplicationController < ActionController::Base
 
   def get_forecast
     location = params["location"].strip
-    #use free API to get global coordinates of given location   
-    error = "" 
-    get_coords = HTTParty.get("https://geocoding-api.open-meteo.com/v1/search?name=#{location}&count=10&language=en&format=json")
-    if get_coords["results"].present?
-      latitude, longitude = get_coords["results"][0]["latitude"], get_coords["results"][0]["longitude"]
-    else
-      # TODO: handle error
-    end
-    #use National Weather Service api to get data endpoint by coordinates
-    url = "https://api.weather.gov/points/#{latitude},#{longitude}"
-    response = HTTParty.get(url)
-    response = JSON.parse(response)
-    if response["status"] == 404
-      # TODO: Return error
-    end
-    forecast_url = response["properties"]["forecast"]
-    #get National Weather Service forecast data for location
-    forecast_data = HTTParty.get(forecast_url)
-    forecast_data = JSON.parse(forecast_data)
+    #use free API to get global coordinates of given location
     results = {}
-    if forecast_data["status"] == 404
-      #TODO handle error
-    else
-      current_data = forecast_data["properties"]["periods"][0]
+    error = false
+    begin
+      latitude, longitude = get_coords(location)
+      #use National Weather Service api to get data endpoint by coordinates
+      forecast_url = get_forecast_endpoint(latitude, longitude)
+      #get National Weather Service forecast data for location
+      current_data = get_forecast_data(forecast_url)
       results["details"] = current_data["detailedForecast"]
       results["temp"] = current_data["temperature"]
+    rescue => e
+      error = true
     end
     render json: {forecast: results, error: error}, status: :ok
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  
+  def get_coords(location_string)
+    latitude, longitude, error
+    coords = HTTParty.get("https://geocoding-api.open-meteo.com/v1/search?name=#{location}&count=1&language=en&format=json")
+    if coords["results"].present?
+      latitude, longitude = coords["results"][0]["latitude"], coords["results"][0]["longitude"]
+    else
+      error = true
+    end
+    latitude, longitude
+  end
+
+  def get_forecast_endpoint(lat,long)
+    url = "https://api.weather.gov/points/#{latitude},#{longitude}"
+    response = HTTParty.get(url)
+    response = JSON.parse(response)
+    forecast_url = response["properties"]["forecast"]
+  end
+    
+  end
+
+  def get_forecast_data(forecast_url)
+    forecast_data = HTTParty.get(forecast_url)
+    forecast_data = JSON.parse(forecast_data)
+    current_data = forecast_data["properties"]["periods"][0] 
+  end
+  
 end
