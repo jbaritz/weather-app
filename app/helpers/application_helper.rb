a8467de82d78fd600f80bef9a9fef4dbcfc88613
jbaritz <jbaritz@gmail.com>
2025-11-15 23:14:52 -0600

    change forecast endpoint to get more accurate data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,10 @@ module ApplicationHelper
     #use National Weather Service api to get data endpoint by coordinates
     forecast_url = get_forecast_endpoint(latitude, longitude)
     #get National Weather Service forecast data for location
-    current_data = get_forecast_data(forecast_url)
+    current_weather = get_forecast_data(forecast_url)
     #return data as array to be mapped to results object
-    [current_data["detailedForecast"], current_data["temperature"]]    
+    details_string = "#{current_weather["shortForecast"]}, #{current_weather["probabilityOfPrecipitation"]["value"]}% chance of precipitation"
+    [details_string, current_weather["temperature"]]
   end
 
   def get_coords(location)
@@ -19,13 +20,13 @@ module ApplicationHelper
     url = "https://api.weather.gov/points/#{latitude},#{longitude}"
     response = HTTParty.get(url)
     response = JSON.parse(response)
-    forecast_url = response["properties"]["forecast"]
+    forecast_url = response["properties"]["forecastHourly"]
   end
 
   def get_forecast_data(forecast_url)
     forecast_data = HTTParty.get(forecast_url)
     forecast_data = JSON.parse(forecast_data)
-    current_data = forecast_data["properties"]["periods"][0] 
+    current_weather = forecast_data["properties"]["periods"][0]
   end
 
 end
